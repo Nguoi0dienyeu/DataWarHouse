ae56046d8105a28d56a209e4c2065d594f027293
Nguoi0dienyeu <mr.itproduction@gmail.com>
2021-07-10 23:33:44 +0700

    fix lai menu mobile

--- js/respone-menu.js
+++ js/respone-menu.js
@@ -1,8 +1,18 @@
-function myFunc() {
-  var x = document.getElementById("topnav");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+// function myFunc() {
+//   var x = document.getElementById("topnav");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+
+// code new close btn menu 
+function closeNav() {
+  document.getElementById("topnav").style.display = "block";
+}
+
+function openNav() {
+  document.getElementById("topnav").style.display = "none";
 }
